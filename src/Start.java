971d96e3987686e170194f80a23888fabf7a4cab
Alberto838 <albert34321@gmail.com>
2021-10-08 13:10:17 +0200

    zadanka

--- src/Start.java
+++ src/Start.java
@@ -1,5 +1,14 @@
 public class Start {
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        //Zadanie 1
+        System.out.println(31+29+31);
+
+        //Zadanie 2
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+
+        //Zadanie 3
+        System.out.println(1*2*3*4*5*6*7*8*9*10);
+
+
     }
 }
