6bcb5e779c4b04af51d42a96f09b69b9d9fcb9b0
Alberto838 <albert34321@gmail.com>
2021-10-16 17:53:55 +0200

    Create L0.java

---
+++ src/L0.java
@@ -0,0 +1,44 @@
+public class L0 {
+    public static void main(String[] args){
+        //Zadanie 1
+        System.out.println(31+29+31);
+
+        //Zadanie 2
+        System.out.println(1+2+3+4+5+6+7+8+9+10);
+
+        //Zadanie 3
+        System.out.println(2*3*4*5*6*7*8*9*10);
+
+        //Zadanie 4
+        System.out.println(1000*1.06 + 1000*1.06*1.06 + 1000*1.06*1.06*1.06);
+
+        //Zadanie 5
+        System.out.println("------");
+        System.out.println("|Java|");
+        System.out.println("------");
+
+        //Zadanie 6
+        System.out.println("  /////");
+        System.out.println(" ±\"\"\"\"\"±");
+        System.out.println("(| ໐ ໐ |)");
+        System.out.println("  | ᐢ |");
+        System.out.println(" | ‘-’ |");
+        System.out.println(" ±-----±");
+
+        //Zadanie 10
+        System.out.println("Rush Hour\n" +
+                "Pixels\n" +
+                "Free guy");
+
+
+        //Zadanie 11
+        System.out.println("Dwadzieścia siedem kości,\n" +
+                "trzydzieści pięć mięśni,\n" +
+                "około dwóch tysięcy komórek nerwowych\n" +
+                "w każdej opuszce naszych pięciu palców.\n" +
+                "To zupełnie wystarczy,\n" +
+                "żeby napisać „Mein Kampf”\n" +
+                "albo „Chatkę Puchatka”.");
+
+    }
+}
